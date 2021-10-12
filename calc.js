0fe574fd6dc79797b74a2d482acbe3b57edaf581
Konstantin Lishenkov <53824925+s0163557@users.noreply.github.com>
2021-10-12 18:39:22 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,18 +2,17 @@ function func() {
   let result;
   let num1 = Number(document.getElementById("num1").value);
   let num2 = Number(document.getElementById("num2").value);
-  if (! /^[0-9]+$/.test(num1) || ! /^[0-9]+$/.test(num2)) {
-    document.getElementById("result").innerHTML = 'В текстовых полях что-то кроме цифр';
+  if (! (/^[0-9]+$/.test(num1) ) || ! (/^[0-9]+$/.test(num2))) {
+  document.getElementById("result").innerHTML = "ошибка";
   }
   else{
     result=num1*num2;
     document.getElementById("result").innerHTML = result;
-    }  
+    }
 }
 
 window.addEventListener("DOMContentLoaded", function(event)  {
-    console.log("DOM complete");
-
+    console.log("DOM gotov");
     let buttonProiz = document.getElementById("func");
     buttonProiz.addEventListener("Click", func);
-})
+});
